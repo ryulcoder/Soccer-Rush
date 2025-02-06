6ad236a0dae3dbf548b3ad3ba6c241a6dccf00b5
MoveHwan <135596820+MoveHwan@users.noreply.github.com>
2025-02-06 17:45:59 +0900

    2025-02-06
    commit

---
+++ Assets/Scripts/CurvedWorld.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CurvedWorld : MonoBehaviour {
+
+    public Vector3 Curvature = new Vector3(0, 0.0005f, 0);
+
+    int CurvatureID;
+
+    private void OnEnable()
+    {
+        CurvatureID = Shader.PropertyToID("_Curvature");
+    }
+
+    void Update()
+    {
+        Shader.SetGlobalVector(CurvatureID, Curvature);
+    }
+}
