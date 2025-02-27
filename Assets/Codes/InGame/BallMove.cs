a26fa574401749e23d68ca95b91cac936dbd1883
ryulcoder <135496109+ryulcoder@users.noreply.github.com>
2025-02-27 22:09:01 +0900

    22

--- Assets/Codes/InGame/BallMove.cs
+++ Assets/Codes/InGame/BallMove.cs
@@ -367,11 +367,11 @@ public class BallMove : MonoBehaviour
         HitDefender(collider);
 
         // 슛하고 공이 다시올때 변수꺼주기
-        if (collider.gameObject.CompareTag("PlayerFoot") && isShooting)
+        if (collider.gameObject.CompareTag("ShootReceive") && isShooting)
         {
             ballReset = true;
             Debug.LogWarning("pos: " + BallTrans.position + " 슛팅 볼 위치 조정");
-            BallTrans.position = new Vector3(0, BallTrans.position.y, PlayerTrans.position.z + 4.5f);
+            //BallTrans.position = new Vector3(0, BallTrans.position.y, PlayerTrans.position.z + 4.5f);
 
             BallRigibody.velocity = Vector3.zero;
             BallRigibody.angularVelocity = Vector3.zero;  // 회전 속도도 초기화
