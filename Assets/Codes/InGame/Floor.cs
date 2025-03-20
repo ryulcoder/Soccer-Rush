0431d805feabf0237f662829e1d540a5c4295e35
MoveHwan <135596820+MoveHwan@users.noreply.github.com>
2025-03-20 18:41:50 +0900

    2025-03-20
    commit

--- Assets/Codes/InGame/Floor.cs
+++ Assets/Codes/InGame/Floor.cs
@@ -15,10 +15,12 @@ public class Floor : MonoBehaviour
 
     public List<Vector3> posList;
 
+    static int fpRanInt = 1;
+    static int fpRangeInt = 1;
     static float leftGap = 0;
     static float prevX = -2;
     static float fpDistance = 0;
-    static bool useLeftGap, prevLineIs2, fixedPattern, fpCoolTimeOn;
+    static bool useLeftGap, prevLineIs2, fixedPattern;
 
     Vector3 floorScale;
     float[] defXs;
@@ -26,7 +28,8 @@ public class Floor : MonoBehaviour
     [SerializeField] string[] defNames = { "StandTackle_Front", "SlidingTackle_Front", "SlidingTackle_Anomaly", "Two_Defenders" , "Three_Defenders", "Three_Defenders_Anomaly" };
 
     [SerializeField] float minGap, maxGap;
-    [SerializeField] bool onPlayer, inPlayer, coroutine, firstSet;
+    [SerializeField] bool onPlayer, inPlayer, coroutine, fpCoolTimeOn;
+    
 
     void Awake()
     {
@@ -44,8 +47,7 @@ public class Floor : MonoBehaviour
         // 시작시 첫 floor 제외 순차적 수비 세팅
         if (otherFloor)
         {
-            SetDefenders();
-            otherFloor.FirstSetting();
+            StartCoroutine(FirstSettingCoroutine());
         }
     }
 
@@ -61,6 +63,8 @@ public class Floor : MonoBehaviour
         if (fixedPattern && !fpCoolTimeOn)
         {
             fpCoolTimeOn = true;
+            fpDistance = GameManager.ScoreCal.Distance;
+
             StartCoroutine(FixedPatternCoolTime());
         }
     }
@@ -84,31 +88,36 @@ public class Floor : MonoBehaviour
 
         inPlayer = false;
 
-        SetDefenders();
+        yield return StartCoroutine(SetDefenders());
 
         coroutine = false;
 
-        PoolManager.LeftObjectDestroy();
+        //PoolManager.LeftObjectDestroy();
 
         yield break;
     }
 
     // 수비 세팅
-    void SetDefenders()
+    IEnumerator SetDefenders()
     {
         (minGap, maxGap) = GameManager.DefGap;
         defPer = GameManager.DefPer;
 
-        List<GameObject> targetObj = new();
+        if (!fixedPattern)
+        {
+            yield return StartCoroutine(FixedPatternSetDefends());
+
+            if (fixedPattern)
+                yield break;
+        }
+
+        List<GameObject> targetObjs = new();
         float[] ranXs;
 
         float targetPos = transform.TransformPoint(new(0, 0, -0.5f)).z;
         float MaxPos = targetPos + floorScale.z;
 
         float ranNum;
-
-        if (!fixedPattern)
-            FixedPatternSetDefends();
 
         // 새로운 수비 리스트 좌표 세팅
         while (true)
@@ -132,7 +141,7 @@ public class Floor : MonoBehaviour
             {
                 targetPos += ranNum;
 
-                posList.Add(new(0, 0.75f, targetPos));
+                posList.Add(new(0, 0.5f, targetPos));
             }
 
         }
@@ -152,7 +161,11 @@ public class Floor : MonoBehaviour
                 string[] orderDefNames = defNames[..3].OrderBy(_ => Random.value).ToArray();
 
                 for (int i = 0; i < orderDefNames.Length; i++)
-                    targetObj.Add(PoolManager.PopObject(orderDefNames[i]));
+                {
+                    targetObjs.Add(PoolManager.PopObject(orderDefNames[i]));
+                    yield return null;
+                }
+                    
             }
             // 3라인 수비 패턴
             else if (defStr == "Three_Defenders")
@@ -165,8 +178,11 @@ public class Floor : MonoBehaviour
                     posList[posIdx] += Vector3.forward * 10;
 
                 for (int i = 0; i < 3; i++)
-                    targetObj.Add(PoolManager.PopObject(defStr));
-
+                {
+                    targetObjs.Add(PoolManager.PopObject(defStr));
+                    yield return null;
+                }
+                    
             }
             // 2라인 수비 패턴
             else if (defStr == "Two_Defenders")
@@ -183,13 +199,15 @@ public class Floor : MonoBehaviour
                     else
                         defStr = defNames[Random.Range(0, 2)];
 
-                    targetObj.Add(PoolManager.PopObject(defStr));
+                    targetObjs.Add(PoolManager.PopObject(defStr));
+                    yield return null;
                 }
             }
             // 1라인
             else
             {
-                targetObj.Add(PoolManager.PopObject(defStr));
+                targetObjs.Add(PoolManager.PopObject(defStr));
+                yield return null;
             }
                 
             
@@ -197,10 +215,10 @@ public class Floor : MonoBehaviour
             Vector3 settingVec;
 
             // 오브젝트pop 만약 프리팹인경우 복제 생성 후 정해진 위치 이동
-            for (int i= 0;i < targetObj.Count; i++)
+            for (int i= 0;i < targetObjs.Count; i++)
             {
                 // 2라인 수비 조정
-                if (prevLineIs2 && targetObj.Count == 2 && i == 1 && ranXs[0] != prevX && ranXs[1] != prevX)
+                if (prevLineIs2 && targetObjs.Count == 2 && i == 1 && ranXs[0] != prevX && ranXs[1] != prevX)
                 {
                     prevX = ranXs[1];
                     ranXs[1] = ranXs[2];
@@ -208,7 +226,7 @@ public class Floor : MonoBehaviour
                 }
 
                 // 1라인
-                if (i == 0 && targetObj.Count == 1)
+                if (i == 0 && targetObjs.Count == 1)
                 {
                     if (!prevLineIs2 && prevX == ranXs[0])
                     {
@@ -226,16 +244,16 @@ public class Floor : MonoBehaviour
                     settingVec = new(ranXs[i], posList[posIdx].y, posList[posIdx].z);
                 }
                     
-                targetObj[i].transform.position = settingVec;
-                targetObj[i].transform.SetParent(transform);
+                targetObjs[i].transform.position = settingVec;
+                targetObjs[i].transform.SetParent(transform);
 
-                string[] nameStrs = targetObj[i].name.Split("_");
-                targetObj[i].name = "Z(" +(posList[posIdx].z - prev) + ")_" + nameStrs[1].Replace("(clone)", "") + "_" + nameStrs[2].Split("(")[0];
+                string[] nameStrs = targetObjs[i].name.Split("_");
+                targetObjs[i].name = "Z(" +(posList[posIdx].z - prev) + ")_" + nameStrs[1].Replace("(clone)", "") + "_" + nameStrs[2].Split("(")[0];
 
-                targetObj[i].SetActive(true);
+                targetObjs[i].SetActive(true);
             }
 
-            if (targetObj.Count == 2)
+            if (targetObjs.Count == 2)
             {
                 prevLineIs2 = true;
                 prevX = ranXs[2];
@@ -246,24 +264,11 @@ public class Floor : MonoBehaviour
             }
 
             prev = posList[posIdx].z;
-            targetObj.Clear();
+            targetObjs.Clear();
         }
 
         posList.Clear();
-
-    }
-
-    void FixedPatternSetDefends()
-    {
-
-    }
-
-    IEnumerator FixedPatternCoolTime()
-    {
-        yield return new WaitUntil(() => fpDistance + 20 < GameManager.ScoreCal.Distance);
-
-        fpCoolTimeOn = false;
-        fixedPattern = false;
+        yield break;
     }
 
     // 랜덤 수비 인덱스 
@@ -271,7 +276,7 @@ public class Floor : MonoBehaviour
     {
         float ranNum = Random.value * 100;
 
-        for (int i = 0;i < defPer.Length; i++) 
+        for (int i = 0; i < defPer.Length; i++)
         {
             if (ranNum - defPer[i] <= 0)
                 return i;
@@ -283,16 +288,200 @@ public class Floor : MonoBehaviour
         return defPer.Length - 1;
     }
 
-    // 시작시 첫 floor 제외 순차적 수비 세팅함수
-    public void FirstSetting()
+
+    IEnumerator FixedPatternSetDefends()
     {
-        if (!firstSet)
+        fpCoolTimeOn = true;
+        fixedPattern = true;
+
+        if (Random.Range(0, fpRanInt) == 0)
         {
-            firstSet = true;
-            SetDefenders();
+            fixedPattern = false;
+            fpCoolTimeOn = false;
+            yield break;
         }
+
+        fpCoolTimeOn = false;
+
+
+        int ranDir = Random.Range(1, 3);
+        if (ranDir == 1) ranDir = 0;
+
+        const int totalDef = 45;
+
+        List<GameObject> fpDefList = new();
+
+        float targetPos = transform.TransformPoint(new(0, 0, -0.5f)).z;
+        float ranNum = Random.Range(minGap, maxGap);
+        int fpXIdx, otherXIdx = 0;
+
+        if (!useLeftGap)
+        {
+            useLeftGap = true;
+            ranNum -= leftGap;
+        }
+
+        targetPos += ranNum;
+
+        // 45
+        switch (Random.Range(0, fpRangeInt))
+        {
+            // 1라인 블락 좌우 회피 전용 고정 패턴
+            case 0:
+
+                if (ranDir == 0)
+                {
+                    fpXIdx = Random.Range(1, 3);
+                    otherXIdx = 2;
+                }
+                else
+                {
+                    fpXIdx = Random.Range(0, 2);
+                    otherXIdx = 0;
+                }
+
+                for (int i = 0; i < totalDef; i++)
+                {
+                    if (i == 0 || i % 5 == 0)
+                    {
+                        fpDefList.Add(PoolManager.PopObject("SlidingTackle_Anomaly"));
+                        posList.Add(new(defXs[fpXIdx], 0.5f, targetPos + 10 * i));
+
+                        if (fpXIdx == 1)
+                            fpXIdx = otherXIdx;
+                        else
+                            fpXIdx = 1;
+                        yield return null;
+                    }
+
+                    if (i == 0 || i % 2 == 0)
+                    {
+                        fpDefList.Add(PoolManager.PopObject("StandTackle_Front"));
+                        posList.Add(new(defXs[ranDir], 0.5f, targetPos + 10 * i));
+                        yield return null;
+                    }
+                }
+
+                break;
+
+            // 2라인 블락 가운데 스킬 회피 전용 고정 패턴
+            case 1:
+                int defRanIdx = 0;
+
+                fpXIdx = Random.Range(0, 2);
+                if (fpXIdx == 1)
+                {
+                    fpXIdx = 2;
+                    otherXIdx = 0;
+                }
+                else
+                    otherXIdx = 2;
+
+
+                for (int i = 0; i < totalDef; i++)
+                {
+                    if (i == 0 || i % 6 == 0)
+                    {
+                        defRanIdx = Random.Range(0, 3);
+
+                        // 변
+                        if (defRanIdx == 2)
+                        {
+                            fpDefList.Add(PoolManager.PopObject("SlidingTackle_Anomaly"));
+                            posList.Add(new(defXs[fpXIdx], 0.5f, targetPos + 10 * i));
+                            yield return null;
+                        }
+                        // 슬
+                        else if (defRanIdx == 1)
+                        {
+                            fpDefList.Add(PoolManager.PopObject("SlidingTackle_Front"));
+                            posList.Add(new(defXs[1], 0.5f, targetPos + 10 * i + 40));
+                            yield return null;
+                        }
+                        // 스
+                        else
+                        {
+                            fpDefList.Add(PoolManager.PopObject("StandTackle_Front"));
+                            posList.Add(new(defXs[1], 0.5f, targetPos + 10 * i));
+                            yield return null;
+                        }
+                        
+                    }
+
+                    // 양 사이드 라인 블락
+                    if (i == 0 || i % 2 == 0)
+                    {
+                        if (defRanIdx == 2)
+                        {
+                            defRanIdx = 0;
+
+                            fpDefList.Add(PoolManager.PopObject("StandTackle_Front"));
+                            posList.Add(new(defXs[otherXIdx], 0.5f, targetPos + 10 * i));
+                            yield return null;
+
+                            (otherXIdx, fpXIdx) = (fpXIdx, otherXIdx);
+                        }
+                        else
+                        {
+                            fpDefList.Add(PoolManager.PopObject("StandTackle_Front"));
+                            posList.Add(new(defXs[0], 0.5f, targetPos + 10 * i));
+                            yield return null;
+
+                            fpDefList.Add(PoolManager.PopObject("StandTackle_Front"));
+                            posList.Add(new(defXs[2], 0.5f, targetPos + 10 * i));
+                            yield return null;
+                        }
+                    }
+                }
+                break;
+
             
+        }
+
+
+        for (int i = 0; i < fpDefList.Count; i++)
+        {
+            fpDefList[i].transform.position = posList[i];
+            fpDefList[i].transform.SetParent(transform);
+
+            string[] nameStrs = fpDefList[i].name.Split("_");
+            fpDefList[i].name = "Z(" + posList[i].z + ")_" + nameStrs[1].Replace("(clone)", "") + "_" + nameStrs[2].Split("(")[0];
+
+            fpDefList[i].SetActive(true);
+
+        }
+
+        posList.Clear();
+        prevX = 0;
     }
+
+    IEnumerator FixedPatternCoolTime()
+    {
+        yield return new WaitUntil(() => fpDistance + 200 < GameManager.ScoreCal.Distance);
+
+        if (fpRangeInt == 1) fpRangeInt = 2;
+
+        fpCoolTimeOn = false;
+        fixedPattern = false;
+    }
+
+    
+
+    // 시작시 첫 floor 제외 순차적 수비 세팅함수
+    void FirstSetting()
+    {
+        StartCoroutine(SetDefenders());
+    }
+
+    IEnumerator FirstSettingCoroutine()
+    {
+        yield return StartCoroutine(SetDefenders());
+
+        fpRanInt = 2;
+
+        otherFloor.FirstSetting();
+    }
+
 
     private void OnTriggerStay(Collider collider)
     {
